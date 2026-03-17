a0a76ea90196519c57bd6b513c9fa87dc21cb1d9
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-03-17 11:24:11 +0100

    Install of Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,10 +12,26 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-    })
+    }),
   );
+  app.enableCors({
+    origin: 'http://localhost:4200', // Frontend URL
+    credentials: true, // Allow cookies or authentication headers
+    methods: 'GET,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization']
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
-  console.log("App login on port ", process.env.PORT )
+  console.log('App login on port ', process.env.PORT);
 }
 bootstrap();
