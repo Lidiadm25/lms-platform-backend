6b9849400c23d71bc9bd3109694c02098630e89a
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-04-08 11:23:37 +0200

    Delete file if its over chosen size

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,85 +1,92 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException, Res} from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors
+} from '@nestjs/common';
 import type { Response } from 'express';
 
-import { FilesService } from './files.service';
-import { diskStorage, Multer } from 'multer';
+import { ConfigService } from '@nestjs/config';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { FilesService } from './files.service';
 import { fileFilter } from './helpers/fileFilter.helper';
 import { fileNamer } from './helpers/fileNamer.helper';
-import { ConfigService } from '@nestjs/config';
-
-
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService,
-    private readonly configService : ConfigService
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
   ) {}
 
-
   @Get('project/:imageName')
-    findProductImage(
+  findProductImage(
     @Res() res: Response,
-    @Param('imageName') imageName:string){
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
 
-      const path = this.filesService.getStaticProductImage( imageName );
-      
-      res.sendFile ( path );
+    res.sendFile(path);
 
-      /*
+    /*
       res.status(403).json({
         ok: false,
         path: path
       }) */
-    }
-  
+  }
 
-
- 
   @Post('project')
-  @UseInterceptors( FileInterceptor('file', {
-    fileFilter: fileFilter,
-    storage: diskStorage({
-      destination: './static/projects',
-      filename: fileNamer
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      storage: diskStorage({
+        destination: './static/projects',
+        filename: fileNamer,
+      }),
     }),
-}))
-
-  uploadProductImage(
-    @UploadedFile() file: Express.Multer.File){
-
-      if(!file){
-        throw new BadRequestException('No hay archivo ')
-      }
-      const secureUrl = `${this.configService.get('HOST_API')}/files/project/${file.filename}`;
-    console.log({secureUrl})
-    return {secureUrl};
+  )
+  uploadProductImage(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('No hay archivo ');
+    }
+    const secureUrl = `${this.configService.get('HOST_API')}/files/project/${file.filename}`;
+    console.log({ secureUrl });
+    return { secureUrl };
   }
 
-   // NO SÉ CÓMO HACERLO!
-   
+  // NO SÉ CÓMO HACERLO!
+
   @Post('lesson')
- @UseInterceptors( FileInterceptor('file', {
-    fileFilter: fileFilter,
-    storage: diskStorage({
-      destination: './static/projects',
-      filename: fileNamer
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      storage: diskStorage({
+        destination: './static/projects',
+        filename: fileNamer,
+      }),
+      limits: { fileSize: 2e9 },
     }),
-    limits : { fileSize: 2e+9}
-}))
-
+  )
   uploadProjectFile(
-    @UploadedFile() file: Express.Multer.File, @Body('maxSize') size:number){
-      
-      if(!file){
-        throw new BadRequestException('No hay archivo ')
-      }
-     if(file.size > +size) {
-        throw new BadRequestException(`File is over max size: ${file.size} > ${size} ` )
-      }
-      const secureUrl = `${this.configService.get('HOST_API')}/files/project/${file.filename}`;
-    console.log({secureUrl})
-     return {secureUrl};
+    @UploadedFile() file: Express.Multer.File,
+    @Body('maxSize') size: number,
+  ) {
+    if (!file) {
+      throw new BadRequestException('No hay archivo ');
+    }
+    const secureUrl = `${this.configService.get('HOST_API')}/files/project/${file.filename}`;
+    if (file.size > +size) {
+      this.filesService.deleteFile(file.filename);
+      throw new BadRequestException(
+        `File is over max size: ${file.size} > ${size} `,
+      );
+    }
+    return { secureUrl };
   }
-    
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -2,20 +2,23 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { existsSync } from 'fs';
 import { join } from 'path';
 
-
 @Injectable()
 export class FilesService {
- 
-  getStaticProductImage(imageName:string){
+  getStaticProductImage(imageName: string) {
+    const path = join(__dirname, '../../static/projects', imageName);
 
-    const path = join(__dirname,'../../static/projects', imageName);
-
-    if(!existsSync(path)){
-      
-      throw new BadRequestException(`No product found with image ${imageName}`)
-    } 
+    if (!existsSync(path)) {
+      throw new BadRequestException(`No product found with image ${imageName}`);
+    }
 
     return path;
+  }
 
+  async deleteFile(fileName: string) {
+    console.log(fileName);
+    const path = join(__dirname, '../../static/projects', fileName);
+    console.log(path);
+    const fs = require('fs').promises;
+    await fs.unlink(path);
   }
 }
