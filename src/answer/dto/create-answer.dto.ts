123a9d8a4d304077dd98b3908f3efab0c18c89ea
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-05-05 14:44:29 +0200

    feat: functionality of answers [wip]

--- src/answer/dto/create-answer.dto.ts
+++ src/answer/dto/create-answer.dto.ts
@@ -1,1 +1,13 @@
-export class CreateAnswerDto {}
+import { IsNotEmpty, IsString, NotContains } from "class-validator";
+
+export class CreateAnswerDto {
+    @IsString()
+    @IsNotEmpty()
+    @NotContains(" ")
+    questionId !:string
+    
+        @IsString()
+    @IsNotEmpty()
+    @NotContains(" ")
+    type !: string;
+}
--- src/answer/entities/answer.entity.ts
+++ src/answer/entities/answer.entity.ts
@@ -1,19 +1,19 @@
 import { Question } from 'src/question/entities/question.entity';
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { TypesAnswers } from '../interfaces/types-answers';
+import { SurveyUser } from 'src/survey-user/entities/survey-user.entity';
 
 @Entity('answers')
 export class Answer {
   @PrimaryGeneratedColumn('uuid')
   id!: string;
 
-  @OneToMany(() => Question, (question) => question.answers)
-  questions!: Question[];
+  @ManyToOne(() => Question, (question) => question.answers)
+  questions!: Question;
 
-  @Column({
-    type: 'set',
-    enum: TypesAnswers,
-    default: [TypesAnswers.checkbox],
-  })
-  type!: string[];
+  @Column('int')
+  rating!: number;
+  
+  @ManyToOne(()=> SurveyUser, response => response.answers)
+  response!: SurveyUser;
 }
