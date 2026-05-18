c0a77cdb1b97b69441bf50daeccab2011b2ec31b
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-05-18 11:07:41 +0200

    feat: new error when someone tries to edit an existing survey

--- src/survey/survey.service.ts
+++ src/survey/survey.service.ts
@@ -1,50 +1,60 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/auth/entities/user.entity';
+import { Project } from 'src/project/entities/project.entity';
+import { Question } from 'src/question/entities/question.entity';
+import { Repository } from 'typeorm';
 import { CreateSurveyDto } from './dto/create-survey.dto';
 import { UpdateSurveyDto } from './dto/update-survey.dto';
-import { User } from 'src/auth/entities/user.entity';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Survey } from './entities/survey.entity';
-import { Repository } from 'typeorm';
-import { Project } from 'src/project/entities/project.entity';
-import { NotFoundError } from 'rxjs';
-import { Question } from 'src/question/entities/question.entity';
 
 @Injectable()
 export class SurveyService {
-
   constructor(
     @InjectRepository(Survey)
     private readonly surveyRepository: Repository<Survey>,
     @InjectRepository(Project)
     private readonly projectRepository: Repository<Project>,
     @InjectRepository(Question)
-    private readonly questionRepository: Repository<Question>
-  ){}
+    private readonly questionRepository: Repository<Question>,
+  ) {}
 
   async create(createSurveyDto: CreateSurveyDto, user: User) {
-    console.log(createSurveyDto)
-    const project = await this.projectRepository.findOne({where : {id: createSurveyDto.idProject}})
+    try {
+      const project = await this.projectRepository.findOne({
+        where: { id: createSurveyDto.projectsId },
+      });
 
-    if(!project) throw new NotFoundError(`Project with id ${createSurveyDto.idProject} doens't exists`)
+      if (!project)
+        throw new NotFoundException(
+          `Project with id ${createSurveyDto.projectsId} doens't exists`,
+        );
 
-    const survey = this.surveyRepository.create({
-      questions: createSurveyDto.questions,
-      user_author: user,
-      projects: project
-    })  
+      const survey = this.surveyRepository.create({
+        questions: createSurveyDto.questions,
+        user_author: user,
+        projects: project,
+      });
 
-    await this.surveyRepository.save(survey)
-
-    console.log("guarda survey")
-
-    return this.surveyRepository.findOne({
-      where: {id: survey.id},
-      relations: {
-        questions:true
+      await this.surveyRepository.save(survey);
+      return this.surveyRepository.findOne({
+        where: { id: survey.id },
+        relations: {
+          questions: true,
+        },
+      });
+    } catch (error: any) {
+      if (error.errno == 1062) {
+        throw new ConflictException(`Course already has a survey.`);
+      } else {
+        throw new InternalServerErrorException(`Please contact admin`);
       }
-    })
-   
-
+    }
   }
 
   findAll() {
@@ -52,9 +62,13 @@ export class SurveyService {
   }
 
   async findOne(id: string) {
-    const survey = await this.surveyRepository.findOne({ where:{projects: {id: id}}, relations: {questions:true} })
+    const survey = await this.surveyRepository.findOne({
+      where: { projects: { id: id } },
+      relations: { questions: true },
+    });
 
-    if(!survey) throw new NotFoundException(`No survey found for this project`)
+    if (!survey)
+      throw new NotFoundException(`No survey found for this project`);
 
     return survey;
   }
@@ -67,10 +81,10 @@ export class SurveyService {
     return `This action removes a #${id} survey`;
   }
 
-
-  async surveyProjectsAuthor(user: User){
-    const query = this.surveyRepository.createQueryBuilder('survey')
-    .where("survey.author = :user",{user: user})
-    .select("avg");
+  async surveyProjectsAuthor(user: User) {
+    const query = this.surveyRepository
+      .createQueryBuilder('survey')
+      .where('survey.author = :user', { user: user })
+      .select('avg');
   }
 }
