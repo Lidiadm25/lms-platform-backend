c0571b882bd36d6b6bde2eb0267e7599eed58580
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-05-18 11:09:54 +0200

    feat: new /Get requests to collect the avg rating of each course and to collect the avg  of an specific course. feat: /post creation of survey

--- src/survey-user/survey-user.controller.ts
+++ src/survey-user/survey-user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { SurveyUserService } from './survey-user.service';
 import { CreateSurveyUserDto } from './dto/create-survey-user.dto';
 import { UpdateSurveyUserDto } from './dto/update-survey-user.dto';
@@ -12,15 +20,30 @@ export class SurveyUserController {
   constructor(private readonly surveyUserService: SurveyUserService) {}
 
   @Post()
-  @Auth(ValidRoles.user)
-  create(@Body() createSurveyUserDto: CreateSurveyUserDto, @GetUser() user: User) {
-
+  @Auth()
+  create(
+    @Body() createSurveyUserDto: CreateSurveyUserDto,
+    @GetUser() user: User,
+  ) {
     return this.surveyUserService.create(createSurveyUserDto, user);
   }
 
   @Get()
   findAll() {
     return this.surveyUserService.findAll();
+  }
+
+  // Que recoja el avg de todas las surveys creadas por el autor
+  @Get('avg')
+  @Auth()
+  getAvgRating(@GetUser() user: User) {
+    return this.surveyUserService.getAvg(user);
+  }
+
+  // Avg rating de un sólo proyecto
+  @Get(':projectId/avg')
+  getAvgSurvey(@Param('projectId') id: string) {
+    return this.surveyUserService.getAvgSpecific(id);
   }
 
   @Get(':id')
@@ -30,7 +53,10 @@ export class SurveyUserController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSurveyUserDto: UpdateSurveyUserDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateSurveyUserDto: UpdateSurveyUserDto,
+  ) {
     return this.surveyUserService.update(+id, updateSurveyUserDto);
   }
 
--- src/survey-user/survey-user.service.ts
+++ src/survey-user/survey-user.service.ts
@@ -1,44 +1,93 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/auth/entities/user.entity';
+import { Survey } from 'src/survey/entities/survey.entity';
+import { Repository } from 'typeorm';
 import { CreateSurveyUserDto } from './dto/create-survey-user.dto';
 import { UpdateSurveyUserDto } from './dto/update-survey-user.dto';
-import { User } from 'src/auth/entities/user.entity';
-import { InjectRepository } from '@nestjs/typeorm';
 import { SurveyUser } from './entities/survey-user.entity';
-import { Not, Repository } from 'typeorm';
-import { Survey } from 'src/survey/entities/survey.entity';
+import { userInfo } from 'os';
 
 @Injectable()
 export class SurveyUserService {
-
   constructor(
     @InjectRepository(SurveyUser)
-    private readonly surveyUserRepository :Repository<SurveyUser>,
+    private readonly surveyUserRepository: Repository<SurveyUser>,
     @InjectRepository(Survey)
-    private readonly surveyRepository : Repository<Survey>
-  ){}
+    private readonly surveyRepository: Repository<Survey>,
+  ) {}
 
-  async create(createSurveyUserDto: CreateSurveyUserDto, user:User) {
+  async create(createSurveyUserDto: CreateSurveyUserDto, user: User) {
+    const survey = await this.surveyRepository.findOneBy({
+      id: createSurveyUserDto.surveyId,
+    });
 
-    const survey = await this.surveyRepository.findOneBy({id: createSurveyUserDto.idSurvey})
-    
-    if(!survey) throw new NotFoundException(`Survey not found`)
+    if (!survey) throw new NotFoundException(`Survey not found`);
+    var total: number = 0;
+    createSurveyUserDto.answers.map((answer) => (total += answer.rating));
 
-  const answer =  this.surveyUserRepository.create({
-      ...createSurveyUserDto,
-      survey: survey
-    })
-    return this.surveyUserRepository.save(answer);
+    total = total / createSurveyUserDto.answers.length;
+    const userAnswer = this.surveyUserRepository.create({
+      user: user,
+      survey: survey,
+      total: total,
+      answers: createSurveyUserDto.answers.map((answer) => ({
+        rating: answer.rating,
+        questions: {
+          id: answer.questionsId,
+        },
+      })),
+    });
+
+    return this.surveyUserRepository.save(userAnswer);
+  }
+
+  async getAvg(author: User) {
+    const results = await this.surveyUserRepository
+      .createQueryBuilder('ratings')
+      .leftJoin('ratings.survey', 'survey')
+      .leftJoin('survey.user_author', 'user_author')
+      .leftJoin('survey.projects', 'projects')
+      .where('user_author.id = :id', { id: author.id })
+      .select('AVG(ratings.total) as avg')
+      .addSelect('projects.id as id')
+      .addSelect('projects.title as course')
+      .distinct(true)
+      .groupBy('course, id')
+      .getRawMany();
+
+    return results;
+  }
+
+  async getAvgSpecific(id: string) {
+    const results = await this.surveyUserRepository
+      .createQueryBuilder('ratings')
+      .leftJoin('ratings.survey', 'survey')
+      .leftJoin('survey.projects', 'projects')
+      .where('projects.id = :id ', { id: id })
+      .select('AVG(ratings.total) as avg')
+      .getRawOne();
+
+    if (results.avg == null) {
+      return 0;
+    }
+
+    return results.avg;
   }
 
   findAll() {
     return `This action returns all surveyUser`;
   }
 
-  async findOne(id: string, user:User) {
-    const surveyUser = await this.surveyUserRepository.findOneBy({user: user, survey: {id: id}})
-    if(!surveyUser) throw new NotFoundException(`User didn't answer the survey`)
+  async findOne(id: string, user: User) {
+    const surveyUser = await this.surveyUserRepository.findOneBy({
+      user: user,
+      survey: { id: id },
+    });
+    if (!surveyUser)
+      throw new NotFoundException(`User didn't answer the survey`);
 
-      return surveyUser;
+    return surveyUser;
   }
 
   update(id: number, updateSurveyUserDto: UpdateSurveyUserDto) {
