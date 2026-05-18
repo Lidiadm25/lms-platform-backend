48d28ca6235b732391e132e0962309b69001e9a5
ldmdt000.edu <ldmdt000.edu@innovasur.com>
2026-05-18 10:58:49 +0200

    feat: added cascaded 'insert' on questions-answers

--- src/answer/entities/answer.entity.ts
+++ src/answer/entities/answer.entity.ts
@@ -1,19 +1,25 @@
 import { Question } from 'src/question/entities/question.entity';
-import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { TypesAnswers } from '../interfaces/types-answers';
 import { SurveyUser } from 'src/survey-user/entities/survey-user.entity';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn
+} from 'typeorm';
 
 @Entity('answers')
 export class Answer {
   @PrimaryGeneratedColumn('uuid')
   id!: string;
 
-  @ManyToOne(() => Question, (question) => question.answers)
+  @ManyToOne(() => Question, (question) => question.answers, {
+    cascade: ['insert'],
+  })
   questions!: Question;
 
   @Column('int')
   rating!: number;
-  
-  @ManyToOne(()=> SurveyUser, response => response.answers)
+
+  @ManyToOne(() => SurveyUser, (response) => response.answers)
   response!: SurveyUser;
 }
